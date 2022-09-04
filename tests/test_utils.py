684991b066389c9d0ce4c10e766d1999640c3f43
Steven Solomon <solomon.steven.m@gmail.com>
2022-09-03 20:09:06 -0400

    Add test to parse title

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,7 @@
 import unittest
+
+import pandas as pd
+
 from utils import *
 
 
@@ -6,6 +9,14 @@ class TestUtils(unittest.TestCase):
     def test_read_file(self):
         file = read_file("tests/fixtures/example_post.md")
         self.assertEqual(file, "some content")
+
+    def test_parse_post(self):
+        file = read_file("tests/fixtures/post.md")
+        empty_frame = pd.DataFrame()
+
+        result = parse_file(empty_frame, file)
+
+        self.assertEqual(result.loc[0, 'title'], "Mocking Functions in Elixir With ExDoubles")
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -1,4 +1,12 @@
+import pandas as pd
+
+
 def read_file(p):
     with open(p) as f:
         lines = f.readlines()
     return ''.join(lines)
+
+
+def parse_file(df, content):
+    content_row = pd.DataFrame({'title': ['Mocking Functions in Elixir With ExDoubles']})
+    return pd.concat([df, content_row], ignore_index=True)
